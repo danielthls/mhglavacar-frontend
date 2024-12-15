634f9c0215449e1f15e97b9a790748edd69997f7
Daniel Thomaz <daniel_thls@yahoo.com.br>
2024-12-15 10:06:19 -0300

    Add Hero Section WIP

--- src/routes/Home/HomeIndex/index.tsx
+++ src/routes/Home/HomeIndex/index.tsx
@@ -1,5 +1,19 @@
+import './styles.css'
+
 export default function HomeIndex() {
     return (
-        <h1>Hello World!</h1>
+        <main>
+            <section id="hero-section">
+                <div className="mhg-section-container">
+                    <div id='hero-text'>
+                        <h1 className="mhg-section-title">AQUI SEU CARRO SAI BRILHANDO</h1>
+                        <p className='mhg-section-text'>Serviços especializados em limpeza automotiva, garantindo cuidado, excelência e o brilho que seu carro merece.</p>
+                    </div>
+                    <div id='hero-section-image'>
+                        <img id='hero-image' src="https://i.imgur.com/iSdx4zT.jpeg" alt="imagem" />
+                    </div>
+                </div>
+            </section>
+        </main>
     )
 }
